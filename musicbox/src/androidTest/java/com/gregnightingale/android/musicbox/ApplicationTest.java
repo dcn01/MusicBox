1c0a90be029b617fee9ddba24acf45a44d76833c
Greg Nightingale <gregnightingale@gmail.com>
2015-12-07 20:00:10 -0500

    tested beat lifecycle events successfully

--- musicbox/src/androidTest/java/com/gregnightingale/android/musicbox/ApplicationTest.java
+++ musicbox/src/androidTest/java/com/gregnightingale/android/musicbox/ApplicationTest.java
@@ -30,13 +30,11 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
         Listener listener = new Listener() {
             @Override
             void onStart(final MusicalObject started) {
-                Log.i("test1", "note started");
                 assert note == started;
             }
 
             @Override
             void onEnd(final MusicalObject ended) {
-                Log.i("test1", "note ended");
                 assert note == ended;
                 latch.countDown();
                 note.removeListener(this);
@@ -47,7 +45,6 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                Log.i("test1", "play note");
                 note.play();
             }
         }).start();
@@ -66,13 +63,11 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
             Listener listener = new Listener() {
                 @Override
                 public void onStart(final MusicalObject started) {
-                    Log.i("testPauseNote", "note started");
                     assert note == started;
                 }
 
                 @Override
                 public void onEnd(final MusicalObject ended) {
-                    Log.i("testPauseNote", "note ended");
                     assert note == ended;
                     note.removeListener(this);
                     latch.countDown();
@@ -80,13 +75,11 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
 
                 @Override
                 public void onPause(final MusicalObject paused) {
-                    Log.i("testPauseNote", "note paused");
                     assert note == paused;
                 }
 
                 @Override
                 void onResume(final MusicalObject resumed) {
-                    Log.i("testPauseNote", "note resumed");
                     assert note == resumed;
                 }
 
@@ -113,14 +106,12 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
                 handler.postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        Log.i("testPauseNote", "pause the note");
                         note.pause();
                     }
                 }, 1000); //pause the note after one second.
                 handler.postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        Log.i("testPauseNote", "resume the note");
                         note.resume();
                     }
                 }, 3000); //resume after 3 seconds, should play 5 more seconds
@@ -136,56 +127,234 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
         assertTrue(latchTriggered);
     }
 
-    public void testBarPlay1() throws Exception {
+    public void testCancelNote() throws Exception {
+
         final CountDownLatch latch = new CountDownLatch(1);
 
-        Note noteA, noteB, noteC, noteD;
+        class LooperThread extends Thread {
 
-        TimeSignature meter = new TimeSignature("4/4");
-        MusicalContext context = new MusicalContext().setTempo(200).setMeter(meter);
+            Listener listener = new Listener() {
+                @Override
+                void onStart(final MusicalObject started) {
+                    assert note == started;
+                }
 
-        noteA = new Note(context, new NoteDefinition("A4", "4"));
-        noteB = new Note(context, new NoteDefinition("B4", "4"));
-        noteC = new Note(context, new NoteDefinition("C5", "4"));
-        noteD = new Note(context, new NoteDefinition("D5", "4"));
+                @Override
+                public void onEnd(final MusicalObject ended) {
+                    assert note != ended;
+                    note.removeListener(this);
+                }
 
-        Beat beat1 = new Beat(context);
-        beat1.add(noteA);
-        Beat beat2 = new Beat(context);
-        beat2.add(noteB);
-        Beat beat3 = new Beat(context);
-        beat3.add(noteC);
-        Beat beat4 = new Beat(context);
-        beat4.add(noteD);
-        final Bar bar = new Bar(context);
-        bar.addBeat(beat1);
-        bar.addBeat(beat2);
-        bar.addBeat(beat3);
-        bar.addBeat(beat4);
+                @Override
+                public void onPause(final MusicalObject paused) {
+                    assert note != paused;
+                }
 
-        final Thread thread = new Thread(new Runnable() {
+                @Override
+                void onResume(final MusicalObject resumed) {
+                    assert note != resumed;
+                }
+
+                @Override
+                void onCancel(final MusicalObject cancelled) {
+                    assert note == cancelled;
+                    latch.countDown();
+                }
+            };
+
+            public LooperThread() {
+            }
+
+            private Note note;
+
             @Override
             public void run() {
+
+                TimeSignature meter = new TimeSignature("4/4");
+                MusicalContext context = new MusicalContext().setTempo(30).setMeter(meter); // each beat is 2 seconds
+                NoteDefinition noteDef = new NoteDefinition("F5", "1"); // play a whole note, should take 8 seconds
+                note = new Note(context, noteDef);
+                note.generateBuffer();
+                note.addListener(listener);
+
                 Looper.prepare();
-                Log.i("test2", "play bar");
-                bar.play();
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        note.cancel();
+                    }
+                }, 1000); // cancel after 1 second.
+                note.play();
                 Looper.loop();
             }
-        });
 
-        bar.addListener(new Listener() {
-            @Override
-            public void onEnd(final MusicalObject ended) {
-                Log.i("test2", ended.toString() + " ended.");
-                assert bar == ended;
-                latch.countDown();
-            }
-        });
+        }
 
-        thread.start();
-        boolean latchTriggered = latch.await(15, TimeUnit.SECONDS);
+        new LooperThread().start();
+
+        boolean latchTriggered = latch.await(12, TimeUnit.SECONDS);
         assertTrue(latchTriggered);
+    }
 
+    public void testBarPlay1() throws Exception {
+        final CountDownLatch noteTestLatch = new CountDownLatch(4);
+        final CountDownLatch beatTestLatch = new CountDownLatch(6);
+
+        class TestThread extends Thread {
+
+            private Note noteA, noteB, noteC, noteD;
+            private Beat beat1, beat2, beat3, beat4;
+            private Bar bar;
+
+            @Override
+            public void run() {
+                TimeSignature meter = new TimeSignature("4/4");
+                MusicalContext context = new MusicalContext().setTempo(30).setMeter(meter);
+
+                noteA = new Note(context, new NoteDefinition("A4", "4"));
+                noteB = new Note(context, new NoteDefinition("B4", "4"));
+                noteC = new Note(context, new NoteDefinition("C5", "4"));
+                noteD = new Note(context, new NoteDefinition("D5", "4"));
+                beat1 = new Beat(context);
+                beat1.add(noteA);
+                beat2 = new Beat(context);
+                beat2.add(noteB);
+                beat3 = new Beat(context);
+                beat3.add(noteC);
+                beat4 = new Beat(context);
+                beat4.add(noteD);
+                bar = new Bar(context);
+                bar.addBeat(beat1);
+                bar.addBeat(beat2);
+                bar.addBeat(beat3);
+                bar.addBeat(beat4);
+
+                Looper.prepare();
+                final Handler handler = new Handler();
+
+                noteA.addListener(new Listener() {
+                    @Override
+                    void onStart(final MusicalObject obj) {
+                        assert obj == noteA;
+                        noteTestLatch.countDown();
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                noteA.pause();
+                            }
+                        }, 500);
+                    }
+
+                    @Override
+                    void onPause(final MusicalObject obj) {
+                        assert obj == noteA;
+                        noteTestLatch.countDown();
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                noteA.resume();
+                            }
+                        }, 175);
+                    }
+
+                    @Override
+                    void onResume(final MusicalObject obj) {
+                        assert obj == noteA;
+                        noteTestLatch.countDown();
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                noteA.cancel();
+                            }
+                        }, 175);
+                    }
+
+                    @Override
+                    void onCancel(final MusicalObject obj) {
+                        assert obj == noteA;
+                        noteTestLatch.countDown();
+                        noteA.removeListener(this);
+                    }
+                });
+
+                final Listener beatTest2Listener = new Listener() {
+                    @Override
+                    void onStart(final MusicalObject obj) {
+                        beatTestLatch.countDown();
+                    }
+
+                    @Override
+                    void onEnd(final MusicalObject obj) {
+                        beatTestLatch.countDown();
+                    }
+                };
+
+                final Listener beatTest1Listener = new Listener() {
+                    @Override
+                    void onStart(final MusicalObject obj) {
+                        assert obj == beat1;
+                        beatTestLatch.countDown();
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                beat1.pause();
+                            }
+                        }, 100);
+                    }
+
+                    @Override
+                    void onPause(final MusicalObject obj) {
+                        assert obj == beat1;
+                        beatTestLatch.countDown();
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                beat1.resume();
+                            }
+                        }, 100);
+                    }
+
+                    @Override
+                    void onCancel(final MusicalObject obj) {
+                        assert obj == beat1;
+                        beatTestLatch.countDown();
+                        beat1.removeListener(this);
+                        beat1.addListener(beatTest2Listener);
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                beat1.play();
+                            }
+                        }, 100);
+                    }
+
+                    @Override
+                    void onResume(final MusicalObject obj) {
+                        assert obj == beat1;
+                        beatTestLatch.countDown();
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                beat1.cancel();
+                            }
+                        }, 100);
+                    }
+                };
+
+
+                beat1.addListener(beatTest1Listener);
+                bar.play();
+                Looper.loop();
+
+            }
+        }
+        new TestThread().start();
+
+        boolean test1pass = noteTestLatch.await(12, TimeUnit.SECONDS);
+        assertTrue(test1pass);
+        boolean test2pass = beatTestLatch.await(12, TimeUnit.SECONDS);
+        assertTrue(test2pass);
     }
 
     public void testXmlMusicReader() throws Exception {
